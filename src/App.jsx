cb366fbe48daffcfb98fc8bfe89b79ed79875e37
unknown <sistemas@VRE_LAB21023.inacap.cl>
2026-06-22 19:51:21 -0400

    cambio título

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Mi Wiki con amor!</h1>
+          <h1>Wiki 2026 WS</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
